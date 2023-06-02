d6446d552a6a49ff9b44ed81a155a6c5b9946b9f
Peter Lucido <petermlucido@gmail.com>
2023-06-02 10:55:54 -0500

    add favicon and logo

--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
 
 # Add the following import
-from django.http import HttpResponse
 
 # Define the home view
 def home(request):
-  return HttpResponse('<h1>Ready Panic</h1>')
+  return render(request, '<h1>Ready Panic</h1>')
 
 def about(request):
   return render(request, 'about.html')
